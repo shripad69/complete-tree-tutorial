aa25308fa409c387b6bce558a119639832fe6fc1
shripad69 <shripadalgude@gmail.com>
2026-04-04 12:26:12 +0530

    preorder recurrsion

--- 02_Preorder_Iterative.cpp
+++ 02_Preorder_Iterative.cpp
@@ -11,18 +11,8 @@ using namespace std;
   };
 
 class Solution {
-
-
-    void preorder (TreeNode * root, vector <int>& ans) {
-        if (root == nullptr) return;
-        ans.push_back(root -> val);
-        preorder(root -> left, ans);
-        preorder(root -> right, ans); 
-    }
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector <int> ans;
-        preorder(root, ans);
-        return ans;
+        
     }
 };
