0abc8c97988e62767cd8c303c3c780e9db4c145e
shripad69 <shripadalgude@gmail.com>
2026-04-05 10:46:54 +0530

    lv order

---
+++ 08_Level_Order_Traversal.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        queue <TreeNode *> q; vector <vector <int>> ans;
+        if (root == nullptr) return ans;
+
+        q.push(root);
+        while (!q.empty()) {
+
+            // get size of curent lv 
+            int sz = q.size();
+            vector <int> lv;
+            for (int i = 0; i < sz; i++) {
+                TreeNode * curr = q.front();
+                q.pop();
+                lv.push_back(curr->val);
+
+                if (curr->left) q.push(curr->left);
+                if (curr->right) q.push(curr->right);
+            }
+            ans.push_back(lv);
+        }
+
+        return ans;
+
+    }
+};
