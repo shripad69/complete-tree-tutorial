b5e159d9fad7f7220215930e15b793fb0cfd491b
shripad69 <shripadalgude@gmail.com>
2026-04-06 11:52:21 +0530

    top order

--- 15_Top_View_Of_Tree.cpp
+++ 15_Top_View_Of_Tree.cpp
@@ -24,28 +24,76 @@ class Node {
 class Solution {
   public:
 
-    void dfs (Node * root, int x, int y, map <int, map <int, vector <int>>>& mpp) {
-        if (root == nullptr) return;
-        mpp[x][y].push_back(root -> data);
+    // LOGIC - 1
+    // using the dfs method 
+
+    // void dfs (Node * root, int x, int y, map <int, map <int, vector <int>>>& mpp) {
+    //     if (root == nullptr) return;
+    //     mpp[x][y].push_back(root -> data);
         
-        dfs (root -> left, x - 1, y + 1, mpp);
-        dfs (root -> right, x + 1, y + 1, mpp);
-    }
-    vector<int> topView(Node *root) {
+    //     dfs (root -> left, x - 1, y + 1, mpp);
+    //     dfs (root -> right, x + 1, y + 1, mpp);
+    // }
+    // vector<int> topView(Node *root) {
+
+    //     vector <int> ans;
+    //     map <int, map <int, vector <int>>> mpp;
+
+    //     dfs(root, 0, 0, mpp);
+
+    //     for (auto i : mpp) {
+    //         // i.first is an vertical col and i.second is a map of levels with nodes
+    //         auto it = i.second.begin();
+    //         // it->first is the first levels for this vertical and it-> second is set
+    //         ans.push_back(*(it -> second.begin()));
+    //     }
+    //     return ans;
+        
+    // }
+
+    vector <int> topView (Node * root) {
+        // now use bfs to rank the nodes with x and y us
+        // also use the set to store which verticals are done 
+        // as we are doing level order the node at the top will be visited first 
+        
+
+        set <int> verticals;
+        map <int, int> mpp;
+
+        queue <pair <Node *, pair <int, int>>> q;
+        q.push({root, {0, 0}});
+
+        while (!q.empty()) {
+
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                auto fr = q.front();
+                q.pop();
+
+                // now fr.fisrt is node itself fr.second.first is the x and fr.second.second is the y 
+                Node * curr = fr.first;
+                int x = fr.second.first;
+                int y = fr.second.second;
+
+                if (!verticals.count(x)) {
+                    mpp[x] = curr -> data;
+                    verticals.insert(x);
+                }
+                if (curr -> left) {
+                    q.push({curr -> left, {x - 1, y + 1}});
+                }
+                if (curr -> right) {
+                    q.push({curr -> right, {x + 1, y + 1}});
+                }
+            }
+        }
 
         vector <int> ans;
-        map <int, map <int, vector <int>>> mpp;
-
-        dfs(root, 0, 0, mpp);
-
         for (auto i : mpp) {
-            // i.first is an vertical col and i.second is a map of levels with nodes
-            auto it = i.second.begin();
-            // it->first is the first levels for this vertical and it-> second is set
-            ans.push_back(*(it -> second.begin()));
+            ans.push_back(i.second);
         }
+
         return ans;
-        
     }
 };
 
