eb0c295c48cd334bb679181ce6a360532b3eb04b
shripad69 <shripadalgude@gmail.com>
2026-04-06 12:35:56 +0530

    right view

--- 17_Right_View_Of_Tree.cpp
+++ 17_Right_View_Of_Tree.cpp
@@ -1,3 +1,12 @@
+// LOGIC - 1
+//     the last node in the level order traversal of the tree is the right view of tree
+//     so do bfs and return the last node of each levels
+
+// LOGIC - 2
+//     we can traverse the tree from right to left
+//     so that we will first get the right most node for each level
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,32 +21,49 @@ struct TreeNode {
 
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector <int> rightView;
-        if (root == nullptr) return rightView;
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector <int> rightView;
+//         if (root == nullptr) return rightView;
 
-        queue <TreeNode * > q;
-        q.push(root);
+//         queue <TreeNode * > q;
+//         q.push(root);
         
-        while (!q.empty()) {
+//         while (!q.empty()) {
 
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                auto fr = q.front();
-                q.pop();
-                if (fr -> left){
-                    q.push(fr -> left);
-                }
+//             int size = q.size();
+//             for (int i = 0; i < size; i++) {
+//                 auto fr = q.front();
+//                 q.pop();
+//                 if (fr -> left){
+//                     q.push(fr -> left);
+//                 }
 
-                if (fr -> right) {
-                    q.push(fr -> right);
-                }
-                // now pushing the last node in the queue cause it is the node in that level so it will give the right view
-                if (i == size - 1) rightView.push_back(fr -> val);
-            }
+//                 if (fr -> right) {
+//                     q.push(fr -> right);
+//                 }
+//                 // now pushing the last node in the queue cause it is the node in that level so it will give the right view
+//                 if (i == size - 1) rightView.push_back(fr -> val);
+//             }
 
+//         }
+
+//         return rightView;
+//     }
+
+    void dfs (TreeNode * node, int level, vector <int>& ans) {
+        if (node == nullptr) return;
+
+        if (level == ans.size()) {
+            ans.push_back(node -> val);
         }
+        dfs (node -> right, level, ans);
+        dfs (node -> left, level, ans);
 
-        return rightView;
+    }
+
+    vector <int> rightSideView (TreeNode * root) {
+        vector <int> ans;
+        dfs(root, 0, ans);
+        return ans;
     }
 };
